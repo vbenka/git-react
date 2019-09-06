d58c6425812a0e5d8e8d263411366fa0e7f44502
vbenka <vladislav.benka@gmail.com>
2019-09-06 19:43:46 +0200

    Updates

---
+++ precache-manifest.22e7d7fade8b61053d2d6f52c105d897.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01390e8f038db26fd0643589648e4fd0",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "29be7bff6d1a56132ef4",
+    "url": "/react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "06d1d597d80f8c9449a3",
+    "url": "/react/static/js/2.3e5cbb7c.chunk.js"
+  },
+  {
+    "revision": "29be7bff6d1a56132ef4",
+    "url": "/react/static/js/main.fb0be65f.chunk.js"
+  },
+  {
+    "revision": "ea55ba829889db5a8122",
+    "url": "/react/static/js/runtime~main.d63ad7a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
